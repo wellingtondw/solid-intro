1b3e365098eb3cbcb39edb0227d79c4f6f539771
Wellington de Lima <wellington.map@hotmail.com>
2021-09-25 18:31:19 -0300

    resolve

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,12 +1,25 @@
-import { Request, Response } from "express";
+import { Request as ExpressRequest, Response } from "express";
 
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+
+interface IRequest extends ExpressRequest {
+  headers: {
+    user_id: string;
+  };
+}
 
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
+  handle(request: IRequest, response: Response): Response {
+    try {
+      const { user_id } = request.headers;
+      const users = this.listAllUsersUseCase.execute({ user_id });
+
+      return response.json(users);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   }
 }
 
